5c1088037c0c60f6131df893e00b469b7c4bdb9b
Tom Daniels <TRDan6577@users.noreply.github.com>
2017-09-10 13:11:22 -0400

    Made mftcsvtostring

---
+++ Convert-MftCsvToString.ps1
@@ -0,0 +1,17 @@
+﻿<#
+Author: Tom Daniels
+File: Convert-MftCsvToString
+Purpose: Outputs the results of the CSV file generated from the Mft2Csv program
+#>
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$FilePath
+)
+
+$csv = Import-Csv -Path $FilePath -Delimiter "|"
+
+# Print out the timestamp of the file, the file type, and the file path
+ForEach ($file in $csv){
+    $file | Select-Object @{l="Timestamp";e={$_.Date.ToString() + " " + $_.Time.ToString()}},
+    @{l="FileType";e={$_.SourceType}},@{l="Path";e={$_.Desc}},MACB | Format-Table *
+}
